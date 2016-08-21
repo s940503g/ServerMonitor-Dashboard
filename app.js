71b1a87ecef075d4fdc0910a1a1255f8f92d50e7
Ubuntu <ubuntu@ip-172-31-20-43.ap-northeast-1.compute.internal>
2016-08-21 02:51:35 +0000

    Add app.js, express module and index.html

---
+++ app.js
@@ -0,0 +1,12 @@
+// require Express
+var express = require('express');
+
+// 實例化一個 Express物件
+var app = express();
+
+app.use(express.static('public'));
+
+
+app.listen(8080, function () {
+    console.log('ready on port 8080');
+});
